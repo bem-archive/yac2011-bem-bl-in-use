b0e19273bfaeb9fb12ff8cf9b43cdef0c4553dca
Elena Gluhova <lento4ka@dhcp-217-96-wifi.yandex.net>
2011-09-17 13:33:44 +0400

    json

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -2,10 +2,10 @@
     block: 'b-page',
     title: 'index',
     head: [
-        { elem: 'css', url: 'index.css'},
-        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8'},
-        { block: 'i-jquery', elem: 'core'},
-        { elem: 'js', url:'index.js'}
+        { elem: 'css', url: 'index.css' },
+        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8' },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url:'index.js' }
     ],
     content: {
         block: 'b-page-inner',
@@ -15,29 +15,25 @@
                 content: 'Шапка'
             },
             {
+                block: 'b-layout-table',
+                mods: { layout: '60-40' },
                 content: {
-                    block: 'b-layout-table',
-                    mods: { layout: '60-40'},
-                    content: {
-                        elem: 'row',
-                        content: [
-                            {
-                                elem: 'cell',
-                                elemMods: { position: 'left'},
-                                content: {
-                                    elem: 'inner',
-                                    content: [
-                                        'здесь будет меню'
-                                    ]
-                                }
-                            },
-                            {
-                                elem: 'cell',
-                                elemMods: { position: 'right'},
-                                content: 'Second cell'
+                    elem: 'row',
+                    content: [
+                        {
+                            elem: 'cell',
+                            elemMods: { position: 'left' },
+                            content: {
+                                elem: 'inner',
+                                content: 'здесь будет меню'
                             }
-                        ]
-                    }
+                        },
+                        {
+                            elem: 'cell',
+                            elemMods: { position: 'right' },
+                            content: 'Second cell'
+                        }
+                    ]
                 }
             }
         ]
