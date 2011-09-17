1bb37217582298b9ebb2e9c50f9c8d98baebea39
Elena Gluhova <lento4ka@dhcp-217-96-wifi.yandex.net>
2011-09-17 13:36:55 +0400

    json

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -3,9 +3,9 @@
     title: 'index',
     head: [
         { elem: 'css', url: 'index.css'},
-        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8'},
-        { block: 'i-jquery', elem: 'core'},
-        { elem: 'js', url:'index.js'}
+        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8' },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url:'index.js' }
     ],
     content: 'content'
 })
