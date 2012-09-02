08bbee45a2ec68fdf941b086042116dc23a6d3da
Vladimir Grinenko <i@tadatuta.com>
2012-09-03 02:25:52 +0300

    extra argument removed

--- pages/.bem/level.js
+++ pages/.bem/level.js
@@ -5,7 +5,7 @@ for (var n in level) exports[n] = level[n];
 
 exports.techs = function() {
 
-    return extend({}, this.__base() || {}, {
+    return extend(this.__base() || {}, {
 
         'bemjson.js': './techs/bemjson.js.js'
 
