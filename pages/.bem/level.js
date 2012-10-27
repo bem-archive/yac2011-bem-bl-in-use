6e50e064af028429fc18692b569aeced1dda05bc
Vladimir Grinenko <i@tadatuta.com>
2012-10-27 05:51:38 +0300

    update level.js, add node_modules to .gitignore

--- pages/.bem/level.js
+++ pages/.bem/level.js
@@ -1,4 +1,29 @@
-var level = require('../../bem-bl/blocks-desktop/.bem/level.js');
-for (var n in level) exports[n] = level[n];
+var extend = require('bem/lib/util').extend,
+    PATH = require('path'),
 
-exports.techs['bemjson.js'] = './techs/bemjson.js.js';
+    BEM_TECHS = '../../bem-bl/blocks-common/i-bem/bem/techs';
+
+exports.getTechs = function() {
+
+    return {
+        'bemjson.js': './techs/bemjson.js.js',
+        'js': 'js-i',
+        'bemhtml': PATH.join(BEM_TECHS, 'bemhtml.js'),
+        'html': PATH.join(BEM_TECHS, 'html.js')
+    };
+
+};
+
+exports.getConfig = function() {
+
+    return extend({}, this.__base() || {}, {
+
+        bundleBuildLevels: this.resolvePaths([
+            '../../bem-bl/blocks-common',
+            '../../bem-bl/blocks-desktop',
+            '../../blocks'
+        ])
+
+    });
+
+};
